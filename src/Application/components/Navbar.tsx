93f54dd7069eab4fec683f49c33b5c34fd48d228
xavierc90 <colombelxavier7@gmail.com>
2024-06-05 00:01:04 +0200

    Change preview.png

--- src/Application/components/Navbar.tsx
+++ src/Application/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="z-50 bg-black/90 text-white uppercase py-6 flex justify-center text-base font-semibold sticky top-0 ">
+    <nav className="z-50 bg-black/90 text-white uppercase py-6 flex justify-center text-sm font-semibold sticky top-0 ">
      <ul className="inline-flex gap-6">
         <li><a href="#">Le restaurant</a></li>
         <li><a href="#menu">La carte</a></li>
